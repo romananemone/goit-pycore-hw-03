b4b015cbf3f4b39aaf8bebe86d687bc01b1c788f
Roman <asdasd@assad.assda>
2026-02-03 22:46:29 +0200

    task_2 solution

---
+++ py_4/task_2.py
@@ -0,0 +1,18 @@
+from random import sample
+
+def get_numbers_ticket(min: int, max: int, quantity: int) -> list:
+    if (
+        min < 1 or 
+        max > 1000 or 
+        quantity < 0 or
+        quantity > (max - min + 1)
+    ):
+        return []
+    
+    return sorted(sample(range(min, max + 1), quantity))
+
+if __name__ == '__main__':
+    print(get_numbers_ticket(15, 21, 0))   # []
+    print(get_numbers_ticket(15, 20, 6))   # [15, 16, 17, 18, 19, 20]
+    print(get_numbers_ticket(20, 15, 5))   # []
+    print(get_numbers_ticket(15, 20, -5))  # []
